67181b9cf81caa232bdeab969884e44a064e17e3
el193945 <laenderquadros@gmail.com>
2018-10-25 16:41:22 -0300

    deploy

--- src/app/controllers/facebotController.js
+++ src/app/controllers/facebotController.js
@@ -28,7 +28,12 @@ router.post('/', (req, res) => {
                 entry.messaging.forEach((event) => {
                     if(event.message){
                         faceBot.enableMarkSeen(event.sender.id);
-                    
+                       // faceBot.sendFirstMenu(event.sender.id);
+                        setTimeout(() => { 
+                            //faceBot.enableTipeOn(event.sender.id);
+                            //faceBot.treatMessage(event);
+                            faceBot.webview(event.sender.id); 
+                        }, 1500);
                         
                     } else {
                         if(event.postback && event.postback.payload){
