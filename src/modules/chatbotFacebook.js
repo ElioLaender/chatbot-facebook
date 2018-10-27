217799a2cbdf953c13a8c7290ddf83e4e655bbb8
el193945 <laenderquadros@gmail.com>
2018-10-27 15:03:59 -0300

    deploy

--- src/modules/chatbotFacebook.js
+++ src/modules/chatbotFacebook.js
@@ -151,40 +151,58 @@ function chatbotFacebook(){
               "type":"template",
               "payload":{
                 "template_type":"generic",
-                "elements":[]
+                "elements":[
+                  {
+                    "title":"Sandália Anabela Bottero",
+                    "subtitle":`💵 R$ 120,00\n💳 ou até 2x de R$ 60,00 s / juros`,
+                    "image_url":"https://api-sale-facebook.herokuapp.com/product/get/image/jhines224.jpg",
+                    "buttons":[
+                    {
+                        "type":"postback",
+                        "title":"📦 Abrir Produto",
+                        "payload":"<POSTBACK_PAYLOAD>"
+                    },
+                    {
+                        "type":"postback",
+                        "title":"❤ Favoritar",
+                        "payload":"<POSTBACK_PAYLOAD>"
+                        }
+                    ]      
+                }
+                ]
               }
             }
           }
       };
 
-      data.forEach((product) => {
-          product.variations.forEach((variation) => {
-            messageData
-            .message
-            .attachment
-            .payload
-            .elements
-            .push(
-              {
-                  "title": product.name,
-                  "subtitle":`💵 R$ ${variation.price}\n💳 ou até 2x de R$ ${(variation.price / 2)} s / juros`,
-                  "image_url":variation.images[0].path,
-                  "buttons":[
-                  {
-                      "type":"postback",
-                      "title":"📦 Abrir Produto",
-                      "payload":"<POSTBACK_PAYLOAD>"
-                  },
-                  {
-                      "type":"postback",
-                      "title":"❤ Favoritar",
-                      "payload":"<POSTBACK_PAYLOAD>"
-                      }
-                  ]      
-              }
-            );
-        }); 
-      });
+      // data.forEach((product) => {
+      //     product.variations.forEach((variation) => {
+      //       messageData
+      //       .message
+      //       .attachment
+      //       .payload
+      //       .elements
+      //       .push(
+      //         {
+      //             "title": product.name,
+      //             "subtitle":`💵 R$ ${variation.price}\n💳 ou até 2x de R$ ${(variation.price / 2)} s / juros`,
+      //             "image_url":variation.images[0].path,
+      //             "buttons":[
+      //             {
+      //                 "type":"postback",
+      //                 "title":"📦 Abrir Produto",
+      //                 "payload":"<POSTBACK_PAYLOAD>"
+      //             },
+      //             {
+      //                 "type":"postback",
+      //                 "title":"❤ Favoritar",
+      //                 "payload":"<POSTBACK_PAYLOAD>"
+      //                 }
+      //             ]      
+      //         }
+      //       );
+      //   }); 
+      // });
 
         this.callSendApi(messageData, 1500);
     };
